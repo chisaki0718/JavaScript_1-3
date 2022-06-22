149b0fcaa8e335b7a1298b95d8b985d23de0386a
chisaki0718 <jobtech.nagamine@gmail.com>
2022-06-22 09:20:25 +0900

    JavaScript課題①-3修正致しました。

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
 //定数
-const tasks = [
-  `=========================`,
-  `現在持っているのタスク一覧`,
-  `=========================`
-];
-const todos = ['掃除', '買い物', '散歩'];
+const title = '========================\n現在持っているタスクの一覧\n========================';
+const tasks = ['掃除', '買い物', '散歩'];
 
 //「最初の段階でコンソールにタスク一覧が表示されている」の処理
-tasks.forEach(value => console.log(value));
-todos.forEach((value, index) => {
+console.log(title);
+
+//タスクリスト用関数
+const taskList = (tasks) => {
+tasks.forEach((value, index) => {
   console.log(`${index}:${value}`);
 });
+};
+taskList(tasks);
 
 //「タスクを入力する欄がある」の定数
-const answer = window.prompt('タスクを入力してください');
+const task = window.prompt('タスクを入力してください');
 
 //タスクの内容を入力するとタスクが追加され、一覧表示される
-const answers = (answer) => {
-  tasks.forEach(value => console.log(value));
-  todos.push(answer);
-  todos.forEach((value, index) => {
-    console.log(`${index}:${value}`);
-  });
+const addTask = (task) => {
+  console.log(title);
+  tasks.push(task);
+  taskList(tasks);
   window.alert('新しいタスクを追加しました。');
 };
-
-answers(answer);
+addTask(task);
 
 //追加後の処理
 const select = window.prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
